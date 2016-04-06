dec9fad86b8a77decf58b26d2b3377e5d26466e8
Simon Dann <simon@photogabble.co.uk>
2016-04-06 15:40:07 +0100

    Adding README and bench script

---
+++ README.md
@@ -0,0 +1,47 @@
+# PHP Performance Benchmark Script
+
+A super simple PHP script for testing PHP performance on various platforms, run it via the command `php bench.php`; example output:
+
+```
+C:\php bench.php
+----------------------------------------------------------------------
+|                        PHP Benchmark Script                        |
+|                     2016-04-06 16:30:21 +02:00                     |
+|              PHP Version: 5.6.8 // OS Version: WINNT               |
+----------------------------------------------------------------------
+|                              Results                               |
+----------------------------------------------------------------------
+| Math Test:                                                         |
+----------------------------------------------------------------------
+|         Min          |         Max          |         Avg          |
+|      52.214 sec      |      52.474 sec      |      52.386 sec      |
+----------------------------------------------------------------------
+| String Test:                                                       |
+----------------------------------------------------------------------
+|         Min          |         Max          |         Avg          |
+|      50.245 sec      |      50.56 sec       |      50.38 sec       |
+----------------------------------------------------------------------
+| Loop Test:                                                         |
+----------------------------------------------------------------------
+|         Min          |         Max          |         Avg          |
+|      5.538 sec       |      5.569 sec       |      5.558 sec       |
+----------------------------------------------------------------------
+| IfElse Test:                                                       |
+----------------------------------------------------------------------
+|         Min          |         Max          |         Avg          |
+|      3.893 sec       |      3.908 sec       |      3.899 sec       |
+----------------------------------------------------------------------
+| Total time taken: 336.667 sec                                      |
+| Peak Memory usage: 279.02MB                                        |
+----------------------------------------------------------------------
+
+```
+
+If you have any additional PHP system performance based tests that you would like to add to the mix, feel free to fork and send a PR.
+
+## Todo
+
+I would like to add disk i/o based performance testing to be able to see how the filesystem performs as that is one of the biggest bottlenecks I have found on certain cloud platforms.
+
+## Not invented here
+This PHP benchmark script was heaviliy influenced by one written by Alessandro Torrisi available [here](http://www.php-benchmark-script.com/).
---
+++ bench.php
@@ -0,0 +1,219 @@
+<?php
+
+/**
+ * Super Simple PHP Benchmark Performance Script
+ * Based upon the PHP benchmark performance script by Alessandro Torrisi available http://www.php-benchmark-script.com/
+ * as such both myself and Alessandro are listed as authors.
+ *
+ * @author Simon Dann <simon.dann@gmail.com>
+ * @author Alessandro Torrisi <info@code24.nl>
+ * @version 1.2
+ * @licence Creative Commons CC BY 4.0 license
+ * @date April 6th, 2016
+ */
+class Benchmark
+{
+    /**
+     * Closure to benchmark
+     *
+     * @var Closure
+     */
+    private $closure;
+
+    /**
+     * Name of benchmark
+     *
+     * @var string
+     */
+    private $name;
+
+    /**
+     * List of timings
+     *
+     * @var array
+     */
+    private $timings = [];
+
+    /**
+     * The current timing batch number, is incrememnted by start on each iteration
+     * @var int
+     */
+    private $batchNumber = 0;
+
+    public function __construct($name, Closure $closure)
+    {
+        $this->name = $name;
+        $this->closure = $closure;
+    }
+
+    public function start()
+    {
+        $time_start = microtime(true);
+        call_user_func_array($this->closure, []);
+        $this->timings[++$this->batchNumber] = (microtime(true) - $time_start);
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getTimings()
+    {
+        return $this->timings;
+    }
+}
+
+$tests = [
+    new Benchmark('Math Test', function ($iterations = 150000) {
+        $functions = [
+            "abs",
+            "acos",
+            "asin",
+            "atan",
+            "bindec",
+            "floor",
+            "exp",
+            "sin",
+            "tan",
+            "pi",
+            "is_finite",
+            "is_nan",
+            "sqrt"
+        ];
+        foreach ($functions as $key => $function) {
+            if (!function_exists($function)) {
+                unset($functions[$key]);
+            }
+        }
+        unset($function, $key);
+
+        for ($i = 0; $i < $iterations; $i++) {
+            foreach ($functions as $function) {
+                $r = call_user_func_array($function, array($i));
+            }
+        }
+    }),
+    new Benchmark('String Test', function ($iterations = 150000) {
+        $functions = [
+            "addslashes",
+            "chunk_split",
+            "metaphone",
+            "strip_tags",
+            "md5",
+            "sha1",
+            "strtoupper",
+            "strtolower",
+            "strrev",
+            "strlen",
+            "soundex",
+            "ord"
+        ];
+        foreach ($functions as $key => $function) {
+            if (!function_exists($function)) {
+                unset($functions[$key]);
+            }
+        }
+        unset($function, $key);
+
+        $string = "the quick brown fox jumps over the lazy dog";
+
+        for ($i = 0; $i < $iterations; $i++) {
+            foreach ($functions as $function) {
+                $r = call_user_func_array($function, array($string));
+            }
+        }
+    }),
+    new Benchmark('Loop Test', function ($iterations = 19000000) {
+        for ($i = 0; $i < $iterations; ++$i) {
+            ;
+        }
+        $i = 0;
+        while ($i < $iterations) {
+            ++$i;
+        }
+    }),
+    new Benchmark('IfElse Test', function ($iterations = 9000000) {
+        for ($i = 0; $i < $iterations; $i++) {
+            if ($i == -1) {
+            } elseif ($i == -2) {
+            } else {
+                if ($i == -3) {
+                }
+            }
+        }
+
+    })
+];
+
+/**
+ * Cli Line Function
+ * @param null $string
+ * @param string $lineEnding
+ * @param int $padding
+ * @param int $lineLength
+ */
+function cli_line($string = null, $padding = STR_PAD_BOTH, $lineEnding = "\n", $lineLength = 70)
+{
+    $line = str_pad("-", $lineLength, "-");
+
+    if (is_null($string)) {
+        echo $line;
+    } else {
+        echo "|" . str_pad($string, ($lineLength - 2), " ", $padding) . "|";
+    }
+
+    echo $lineEnding;
+}
+
+//
+// Welcome header
+//
+
+cli_line();
+cli_line("PHP Benchmark Script");
+cli_line(date('Y-m-d H:i:s P'));
+cli_line("PHP Version: " . PHP_VERSION . ' // OS Version: ' . PHP_OS);
+cli_line();
+
+//
+// Run the benchmarks
+//
+
+/**@var Benchmark $test */
+for ($i = 1; $i <= 3; $i++) {
+    foreach ($tests as $test) {
+        cli_line(" Executing test [" . $test->getName() . " run #" . $i . "]", STR_PAD_RIGHT, "\r");
+        $test->start();
+    }
+}
+
+//
+// Output the results
+//
+
+cli_line("Results");
+$totalTime = 0;
+
+foreach ($tests as $test) {
+    cli_line();
+    $timings = $test->getTimings();
+    cli_line(" " . $test->getName() . ":", STR_PAD_RIGHT);
+    cli_line();
+
+    $min = round(min($timings), 3) . " sec";
+    $max = round(max($timings), 3) . " sec";
+    $avg = round((array_sum($timings) / count($timings)), 3) . " sec";
+
+    cli_line(str_pad('Min', 22, " ", STR_PAD_BOTH) . '|' . str_pad('Max', 22, " ", STR_PAD_BOTH) . '|' . str_pad('Avg',
+            22, " ", STR_PAD_BOTH), STR_PAD_RIGHT);
+    cli_line(str_pad($min, 22, " ", STR_PAD_BOTH) . '|' . str_pad($max, 22, " ", STR_PAD_BOTH) . '|' . str_pad($avg, 22,
+            " ", STR_PAD_BOTH), STR_PAD_RIGHT);
+
+    $totalTime += array_sum($timings);
+}
+
+cli_line();
+cli_line(" Total time taken: " . round($totalTime, 3) . " sec", STR_PAD_RIGHT);
+cli_line(" Peak Memory usage: " . round(memory_get_peak_usage() / 1024, 2) . "MB", STR_PAD_RIGHT);
+cli_line();
